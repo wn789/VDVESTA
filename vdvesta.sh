f8b85652f71e46d95fb23d476b182090b70addab
OpenShift System User <5707c5f989f5cff00800011b@ex-std-node834.prod.rhcloud.com>
2017-05-17 03:39:50 -0400

    duy commit

--- vdvesta.sh
+++ vdvesta.sh
@@ -553,6 +553,25 @@ service httpd restart >/dev/null 2>&1
 httpd -v
 fi
 if [ "$Web_Server_version" = "--nginx yes --apache no --phpfpm yes" ]; then
+echo '[%backend%]
+listen = /var/run/php5-%backend%.sock
+listen.allowed_clients = 127.0.0.1
+user = %user%
+group = %user%
+listen.owner = %user%
+listen.group = nginx
+pm = ondemand
+pm.max_children = 50
+pm.start_servers = 3
+pm.min_spare_servers = 2
+pm.max_spare_servers = 10
+pm.process_idle_timeout = 10s
+pm.max_requests = 500
+env[HOSTNAME] = $HOSTNAME
+env[PATH] = /usr/local/bin:/usr/bin:/bin
+env[TMP] = /tmp
+env[TMPDIR] = /tmp
+env[TEMP] = /tmp' > /usr/local/vesta/data/templates/web/php-fpm/vDDoS-Socket.tpl
 chkconfig httpd off >/dev/null 2>&1
 service nginx restart >/dev/null 2>&1
 nginx -v
